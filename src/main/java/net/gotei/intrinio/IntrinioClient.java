518495c480887851cbbe7b79f0cacc4334f3feb1
Anton Chankin <chankin@wellgard.org>
2017-04-04 10:20:25 +0300

    #1 Initializes main class

---
+++ src/main/java/net/gotei/intrinio/IntrinioClient.java
@@ -0,0 +1,16 @@
+package net.gotei.intrinio;
+
+/**
+ * Main class
+ */
+public class IntrinioClient {
+    private final String USERNAME;
+    private final String PASSWORD;
+
+    public IntrinioClient(String USERNAME, String PASSWORD) {
+        this.USERNAME = USERNAME;
+        this.PASSWORD = PASSWORD;
+    }
+
+    
+}
