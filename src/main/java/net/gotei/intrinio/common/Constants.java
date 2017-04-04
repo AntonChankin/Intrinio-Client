a8a2589b6d973af1d4839106d65c9a7674a5455d
Anton Chankin <chankin@wellgard.org>
2017-04-04 10:45:13 +0300

    #2 Call limits

---
+++ src/main/java/net/gotei/intrinio/common/Constants.java
@@ -0,0 +1,22 @@
+package net.gotei.intrinio.common;
+
+/**
+ * Constant values common for all API calls.
+ */
+public class Constants {
+    // Paging Limits
+    private static final int pricesEndpointLimit = 50000;
+    private static final int historical_dataEndpointLimit = 50000;
+    private static final int tagsStandardizedEndpointLimit = 5000;
+    private static final int tagsReportedEndpointLimit = 5000;
+    private static final int tagsBanksEndpointLimit = 5000;
+    private static final int financialsStandardizedEndpointLimit = 5000;
+    private static final int financialsReportedEndpointLimit = 5000;
+    private static final int financialsBanksEndpointLimit = 5000;
+    private static final int analyst_ratingsEndpointLimit = 100;
+    private static final int blogger_ratingsEndpointLimit = 100;
+    private static final int news_sentimentsEndpointLimit = 100;
+    private static final int news_sector_sentimentsEndpointLimit = 100;
+    private static final int fileDownloadEndpointLimit = 10000;
+    private static final int othersEndpointLimit = 250;
+}
