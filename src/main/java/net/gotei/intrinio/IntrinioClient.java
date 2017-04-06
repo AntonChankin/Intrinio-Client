a693e706b0e42cc006f93576ae18e8bd01f8fe22
Anton Chankin <this@antonchankin.com>
2017-04-06 13:43:33 +0300

    Adds javadoc to main class #1

--- src/main/java/net/gotei/intrinio/IntrinioClient.java
+++ src/main/java/net/gotei/intrinio/IntrinioClient.java
@@ -1,6 +1,5 @@
 package net.gotei.intrinio;
 
-import net.gotei.intrinio.common.Connector;
 import net.gotei.intrinio.common.Constants;
 import net.gotei.intrinio.common.Extractor;
 import net.gotei.intrinio.common.PagedResponse;
@@ -13,18 +12,30 @@ import java.util.*;
 
 /**
  * Main class
+ * @author Anton Chankin
+ * @version 0.0.1
  */
 public class IntrinioClient {
     private final String USERNAME;
     private final String PASSWORD;
     private Extractor extractor;
 
+    /**
+     * Only constructor.
+     * @param USERNAME Intrinio Access Key's username
+     * @param PASSWORD Intrinio Access Key's password
+     */
     public IntrinioClient(String USERNAME, String PASSWORD) {
         this.USERNAME = USERNAME;
         this.PASSWORD = PASSWORD;
         this.extractor = new Extractor(USERNAME, PASSWORD);
     }
 
+    /**
+     * Return today’s current usage and limit information for the given access code.
+     * @param access_code the access code
+     * @return pairs date and usage
+     */
     public List<HistoricalCell> getHistoricalUsage(String access_code){
         List<HistoricalCell> cells = new ArrayList<HistoricalCell>();
         Map<String,String> param = new HashMap<String, String>(1);
@@ -44,14 +55,29 @@ public class IntrinioClient {
         return cells;
     }
 
+    /**
+     * Returns a list of your authorized access codes and their daily limits.
+     * @return limits per access code
+     */
     public List<AccessLimits> getAccessLimits(){
         return extractor.getAccessLimits();
     }
 
+    /**
+     * Return today’s current usage and limit information for the given access code.
+     * @param access_code the access code
+     * @return limits per access code
+     */
     public List<CurrentLimit> getCurrentUsage(String access_code) {
         return extractor.getCurrentUsage(access_code);
     }
 
+    /**
+     * Returns the master list of all companies covered by the Intrinio Data Marketplace.
+     * @param query (optional, returns full list of companies with compacted response values, if no query specified) - a string query search of company name or ticker symbol with the returned results being the relevant companies in compacted list format.
+     * @param latest_filing_date a date value that returns the list of companies whose latest SEC filing was filed on or after this date: YYYY-MM-DD
+     * @return list of companies
+     */
     public List<Company> getCompanies(String query, String latest_filing_date ){
         List<Company> companies = new ArrayList<Company>();
         Map<String,String> param = new HashMap<String, String>(1);
