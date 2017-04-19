208e0e6011c40fd7ce13cd9f8b131216e3d3d73e
Anton Chankin <chankin@wellgard.org>
2017-04-19 10:36:48 +0300

    #12 Adds Company and Security

--- src/main/java/com/intrinio/us/company/Company.java
+++ src/main/java/com/intrinio/us/company/Company.java
@@ -1,7 +1,390 @@
 package com.intrinio.us.company;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * http://docs.intrinio.com/#companies
  */
 public class Company {
+    /** - the stock market ticker symbol associated with the companies common stock securities */
+    private String ticker;
+    /** - the Central Index Key issued by the SEC, which is the unique identifier all company filings are issued under */
+    private String cik;
+    /** - the company name in shorter form */
+    private String name;
+    /** - the company’s official legal name */
+    private String legal_name;
+    /** - the Legal Entity Identifier for the company */
+    private String lei;
+    /** - the company’s headquarters address line 1 */
+    private String hq_address1;
+    /** - the company’s headquarters address line 2 */
+    private String hq_address2;
+    /** - the company’s headquarters city */
+    private String hq_address_city;
+    /** - the company’s headquarters postal code */
+    private String hq_address_postal_code;
+    /** - the company’s legal organization form */
+    private String entity_legal_form;
+    /** - the state (US & Canada Only) where the company headquarters is located */
+    private String state;
+    /** - the country where the company headquarters is located */
+    private String country;
+    /** - the state (US & Canada Only) where the company is incorporated */
+    private String inc_state;
+    /** - the country where the company is incorporated */
+    private String inc_country;
+    /** - the Standard Industrial Classification (SIC) determined by the company filed with the SEC */
+    private BigDecimal sic;
+    /** - the Stock Exchange where the company’s common stock is primarily traded */
+    private String stock_exchange;
+    /** - the financial statement template used by Intrinio to standardize the as reported data */
+    private String template;
+    /** - a one or two sentence description of the company’s operations */
+    private String short_description;
+    /** - a one paragraph description of the company’s operations and other corporate actions */
+    private String long_description;
+    /** - the Chief Executive Officer of the company */
+    private String ceo;
+    /** - the url to the company’s primary corporate website or primary internet property */
+    private String company_url;
+    /** - the company’s headquarters address */
+    private String business_address;
+    /** - the mailing address reported by the company */
+    private String mailing_address;
+    /** - the phone number reported by the company */
+    private String business_phone_no;
+    /** - the number of employees working for the company */
+    private BigDecimal employees;
+    /** - the company’s operating sector */
+    private String sector;
+    /** - the company’s operating industry category */
+    private String industry_category;
+    /** - the company’s operating industry group */
+    private String industry_group;
+    /** - if true, the company has standardized and as reported fundamental data via the Intrinio API; if false, the company has as reported data only.*/
+    private Boolean standardized_active;
+    /** List of Securities*/
+    private List<Security> securities;
+
+    public String getTicker() {
+        return ticker;
+    }
+
+    public void setTicker(String ticker) {
+        this.ticker = ticker;
+    }
+
+    public String getCik() {
+        return cik;
+    }
+
+    public void setCik(String cik) {
+        this.cik = cik;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLegal_name() {
+        return legal_name;
+    }
+
+    public void setLegal_name(String legal_name) {
+        this.legal_name = legal_name;
+    }
+
+    public String getLei() {
+        return lei;
+    }
+
+    public void setLei(String lei) {
+        this.lei = lei;
+    }
+
+    public String getHq_address1() {
+        return hq_address1;
+    }
+
+    public void setHq_address1(String hq_address1) {
+        this.hq_address1 = hq_address1;
+    }
+
+    public String getHq_address2() {
+        return hq_address2;
+    }
+
+    public void setHq_address2(String hq_address2) {
+        this.hq_address2 = hq_address2;
+    }
+
+    public String getHq_address_city() {
+        return hq_address_city;
+    }
+
+    public void setHq_address_city(String hq_address_city) {
+        this.hq_address_city = hq_address_city;
+    }
+
+    public String getHq_address_postal_code() {
+        return hq_address_postal_code;
+    }
+
+    public void setHq_address_postal_code(String hq_address_postal_code) {
+        this.hq_address_postal_code = hq_address_postal_code;
+    }
+
+    public String getEntity_legal_form() {
+        return entity_legal_form;
+    }
+
+    public void setEntity_legal_form(String entity_legal_form) {
+        this.entity_legal_form = entity_legal_form;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getInc_state() {
+        return inc_state;
+    }
+
+    public void setInc_state(String inc_state) {
+        this.inc_state = inc_state;
+    }
+
+    public String getInc_country() {
+        return inc_country;
+    }
+
+    public void setInc_country(String inc_country) {
+        this.inc_country = inc_country;
+    }
+
+    public BigDecimal getSic() {
+        return sic;
+    }
+
+    public void setSic(BigDecimal sic) {
+        this.sic = sic;
+    }
+
+    public String getStock_exchange() {
+        return stock_exchange;
+    }
+
+    public void setStock_exchange(String stock_exchange) {
+        this.stock_exchange = stock_exchange;
+    }
+
+    public String getTemplate() {
+        return template;
+    }
+
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
+    public String getShort_description() {
+        return short_description;
+    }
+
+    public void setShort_description(String short_description) {
+        this.short_description = short_description;
+    }
+
+    public String getLong_description() {
+        return long_description;
+    }
+
+    public void setLong_description(String long_description) {
+        this.long_description = long_description;
+    }
+
+    public String getCeo() {
+        return ceo;
+    }
+
+    public void setCeo(String ceo) {
+        this.ceo = ceo;
+    }
+
+    public String getCompany_url() {
+        return company_url;
+    }
+
+    public void setCompany_url(String company_url) {
+        this.company_url = company_url;
+    }
+
+    public String getBusiness_address() {
+        return business_address;
+    }
+
+    public void setBusiness_address(String business_address) {
+        this.business_address = business_address;
+    }
+
+    public String getMailing_address() {
+        return mailing_address;
+    }
+
+    public void setMailing_address(String mailing_address) {
+        this.mailing_address = mailing_address;
+    }
+
+    public String getBusiness_phone_no() {
+        return business_phone_no;
+    }
+
+    public void setBusiness_phone_no(String business_phone_no) {
+        this.business_phone_no = business_phone_no;
+    }
+
+    public BigDecimal getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(BigDecimal employees) {
+        this.employees = employees;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+    public void setSector(String sector) {
+        this.sector = sector;
+    }
+
+    public String getIndustry_category() {
+        return industry_category;
+    }
+
+    public void setIndustry_category(String industry_category) {
+        this.industry_category = industry_category;
+    }
+
+    public String getIndustry_group() {
+        return industry_group;
+    }
+
+    public void setIndustry_group(String industry_group) {
+        this.industry_group = industry_group;
+    }
+
+    public Boolean getStandardized_active() {
+        return standardized_active;
+    }
+
+    public void setStandardized_active(Boolean standardized_active) {
+        this.standardized_active = standardized_active;
+    }
+
+    public List<Security> getSecurities() {
+        return securities;
+    }
+
+    public void setSecurities(List<Security> securities) {
+        this.securities = securities;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Company)) return false;
+        Company company = (Company) o;
+        return Objects.equals(ticker, company.ticker) &&
+                Objects.equals(cik, company.cik) &&
+                Objects.equals(name, company.name) &&
+                Objects.equals(legal_name, company.legal_name) &&
+                Objects.equals(lei, company.lei) &&
+                Objects.equals(hq_address1, company.hq_address1) &&
+                Objects.equals(hq_address2, company.hq_address2) &&
+                Objects.equals(hq_address_city, company.hq_address_city) &&
+                Objects.equals(hq_address_postal_code, company.hq_address_postal_code) &&
+                Objects.equals(entity_legal_form, company.entity_legal_form) &&
+                Objects.equals(state, company.state) &&
+                Objects.equals(country, company.country) &&
+                Objects.equals(inc_state, company.inc_state) &&
+                Objects.equals(inc_country, company.inc_country) &&
+                Objects.equals(sic, company.sic) &&
+                Objects.equals(stock_exchange, company.stock_exchange) &&
+                Objects.equals(template, company.template) &&
+                Objects.equals(short_description, company.short_description) &&
+                Objects.equals(long_description, company.long_description) &&
+                Objects.equals(ceo, company.ceo) &&
+                Objects.equals(company_url, company.company_url) &&
+                Objects.equals(business_address, company.business_address) &&
+                Objects.equals(mailing_address, company.mailing_address) &&
+                Objects.equals(business_phone_no, company.business_phone_no) &&
+                Objects.equals(employees, company.employees) &&
+                Objects.equals(sector, company.sector) &&
+                Objects.equals(industry_category, company.industry_category) &&
+                Objects.equals(industry_group, company.industry_group) &&
+                Objects.equals(standardized_active, company.standardized_active) &&
+                Objects.equals(securities, company.securities);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ticker, cik, name, legal_name, lei, hq_address1, hq_address2, hq_address_city, hq_address_postal_code, entity_legal_form, state, country, inc_state, inc_country, sic, stock_exchange, template, short_description, long_description, ceo, company_url, business_address, mailing_address, business_phone_no, employees, sector, industry_category, industry_group, standardized_active, securities);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Company{");
+        sb.append("ticker='").append(ticker).append('\'');
+        sb.append(", cik='").append(cik).append('\'');
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", legal_name='").append(legal_name).append('\'');
+        sb.append(", lei='").append(lei).append('\'');
+        sb.append(", hq_address1='").append(hq_address1).append('\'');
+        sb.append(", hq_address2='").append(hq_address2).append('\'');
+        sb.append(", hq_address_city='").append(hq_address_city).append('\'');
+        sb.append(", hq_address_postal_code='").append(hq_address_postal_code).append('\'');
+        sb.append(", entity_legal_form='").append(entity_legal_form).append('\'');
+        sb.append(", state='").append(state).append('\'');
+        sb.append(", country='").append(country).append('\'');
+        sb.append(", inc_state='").append(inc_state).append('\'');
+        sb.append(", inc_country='").append(inc_country).append('\'');
+        sb.append(", sic=").append(sic);
+        sb.append(", stock_exchange='").append(stock_exchange).append('\'');
+        sb.append(", template='").append(template).append('\'');
+        sb.append(", short_description='").append(short_description).append('\'');
+        sb.append(", long_description='").append(long_description).append('\'');
+        sb.append(", ceo='").append(ceo).append('\'');
+        sb.append(", company_url='").append(company_url).append('\'');
+        sb.append(", business_address='").append(business_address).append('\'');
+        sb.append(", mailing_address='").append(mailing_address).append('\'');
+        sb.append(", business_phone_no='").append(business_phone_no).append('\'');
+        sb.append(", employees=").append(employees);
+        sb.append(", sector='").append(sector).append('\'');
+        sb.append(", industry_category='").append(industry_category).append('\'');
+        sb.append(", industry_group='").append(industry_group).append('\'');
+        sb.append(", standardized_active=").append(standardized_active);
+        sb.append(", securities=").append(securities);
+        sb.append('}');
+        return sb.toString();
+    }
 }
--- src/main/java/com/intrinio/us/company/Security.java
+++ src/main/java/com/intrinio/us/company/Security.java
@@ -1,7 +1,245 @@
 package com.intrinio.us.company;
 
+import java.util.Objects;
+
 /**
+ * Returns security list and information for all securities covered by Intrinio.
  * http://docs.intrinio.com/#securities
  */
 public class Security {
+    /** - the stock market ticker symbol associated with the companies common stock securities */
+    private String ticker;
+    /** - the OpenFIGI ticker symbol */
+    private String figi_ticker;
+    /** - the OpenFIGI identifier */
+    private String figi;
+    /** - the security description as provided by the exchange */
+    private String security_name;
+    /** - the type of market for the security */
+    private String market_sector;
+    /** - the type of security, such as Common Stock, Preferred Stock, Warrants, Limited Partnership Interests, etc. */
+    private String security_type;
+    /** - the Stock Exchange (and market category) where the company’s common stock is primarily traded */
+    private String stock_exchange;
+    /** - the last recorded date (“YYYY-MM-DD”) of an CRSP adjustment made to prior prices due to a stock split or dividend event.*/
+    private String last_crsp_adj_date;
+    /** - the security’s composite FIGI identifier */
+    private String composite_figi;
+    /** - the security’s unique FIGI identifier */
+    private String figi_uniqueid;
+    /** - the security’s share class FIGI identifier */
+    private String share_class_figi;
+    /** - the security’s FIGI country of the exchange */
+    private String figi_exch_cntry;
+    /** - the security’s traded currency on the listed exchange */
+    private String currency;
+    /** - the Market Identification Code for the Stock Exchange where the security is listed. */
+    private String mic;
+    /** - the Intrinio Stock Exchange Symbol used as an identifier on the Intrinio API. */
+    private String exch_symbol;
+    /** - a boolean to represent whether the security is an ETF or otherwise */
+    private Boolean etf;
+    /**- if the security is no longer traded on public exchanges, the security will be considered delisted and the security no longer will report pricing data.*/
+    private Boolean delisted_security;
+    /** - a boolean to represent whether the security is the primary issue or a secondary issue of the security on a different stock exchange. */
+    private Boolean primary_listing;
+
+    public String getTicker() {
+        return ticker;
+    }
+
+    public void setTicker(String ticker) {
+        this.ticker = ticker;
+    }
+
+    public String getFigi_ticker() {
+        return figi_ticker;
+    }
+
+    public void setFigi_ticker(String figi_ticker) {
+        this.figi_ticker = figi_ticker;
+    }
+
+    public String getFigi() {
+        return figi;
+    }
+
+    public void setFigi(String figi) {
+        this.figi = figi;
+    }
+
+    public String getSecurity_name() {
+        return security_name;
+    }
+
+    public void setSecurity_name(String security_name) {
+        this.security_name = security_name;
+    }
+
+    public String getMarket_sector() {
+        return market_sector;
+    }
+
+    public void setMarket_sector(String market_sector) {
+        this.market_sector = market_sector;
+    }
+
+    public String getSecurity_type() {
+        return security_type;
+    }
+
+    public void setSecurity_type(String security_type) {
+        this.security_type = security_type;
+    }
+
+    public String getStock_exchange() {
+        return stock_exchange;
+    }
+
+    public void setStock_exchange(String stock_exchange) {
+        this.stock_exchange = stock_exchange;
+    }
+
+    public String getLast_crsp_adj_date() {
+        return last_crsp_adj_date;
+    }
+
+    public void setLast_crsp_adj_date(String last_crsp_adj_date) {
+        this.last_crsp_adj_date = last_crsp_adj_date;
+    }
+
+    public String getComposite_figi() {
+        return composite_figi;
+    }
+
+    public void setComposite_figi(String composite_figi) {
+        this.composite_figi = composite_figi;
+    }
+
+    public String getFigi_uniqueid() {
+        return figi_uniqueid;
+    }
+
+    public void setFigi_uniqueid(String figi_uniqueid) {
+        this.figi_uniqueid = figi_uniqueid;
+    }
+
+    public String getShare_class_figi() {
+        return share_class_figi;
+    }
+
+    public void setShare_class_figi(String share_class_figi) {
+        this.share_class_figi = share_class_figi;
+    }
+
+    public String getFigi_exch_cntry() {
+        return figi_exch_cntry;
+    }
+
+    public void setFigi_exch_cntry(String figi_exch_cntry) {
+        this.figi_exch_cntry = figi_exch_cntry;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getMic() {
+        return mic;
+    }
+
+    public void setMic(String mic) {
+        this.mic = mic;
+    }
+
+    public String getExch_symbol() {
+        return exch_symbol;
+    }
+
+    public void setExch_symbol(String exch_symbol) {
+        this.exch_symbol = exch_symbol;
+    }
+
+    public Boolean getEtf() {
+        return etf;
+    }
+
+    public void setEtf(Boolean etf) {
+        this.etf = etf;
+    }
+
+    public Boolean getDelisted_security() {
+        return delisted_security;
+    }
+
+    public void setDelisted_security(Boolean delisted_security) {
+        this.delisted_security = delisted_security;
+    }
+
+    public Boolean getPrimary_listing() {
+        return primary_listing;
+    }
+
+    public void setPrimary_listing(Boolean primary_listing) {
+        this.primary_listing = primary_listing;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Security)) return false;
+        Security security = (Security) o;
+        return Objects.equals(ticker, security.ticker) &&
+                Objects.equals(figi_ticker, security.figi_ticker) &&
+                Objects.equals(figi, security.figi) &&
+                Objects.equals(security_name, security.security_name) &&
+                Objects.equals(market_sector, security.market_sector) &&
+                Objects.equals(security_type, security.security_type) &&
+                Objects.equals(stock_exchange, security.stock_exchange) &&
+                Objects.equals(last_crsp_adj_date, security.last_crsp_adj_date) &&
+                Objects.equals(composite_figi, security.composite_figi) &&
+                Objects.equals(figi_uniqueid, security.figi_uniqueid) &&
+                Objects.equals(share_class_figi, security.share_class_figi) &&
+                Objects.equals(figi_exch_cntry, security.figi_exch_cntry) &&
+                Objects.equals(currency, security.currency) &&
+                Objects.equals(mic, security.mic) &&
+                Objects.equals(exch_symbol, security.exch_symbol) &&
+                Objects.equals(etf, security.etf) &&
+                Objects.equals(delisted_security, security.delisted_security) &&
+                Objects.equals(primary_listing, security.primary_listing);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ticker, figi_ticker, figi, security_name, market_sector, security_type, stock_exchange, last_crsp_adj_date, composite_figi, figi_uniqueid, share_class_figi, figi_exch_cntry, currency, mic, exch_symbol, etf, delisted_security, primary_listing);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Security{");
+        sb.append("ticker='").append(ticker).append('\'');
+        sb.append(", figi_ticker='").append(figi_ticker).append('\'');
+        sb.append(", figi='").append(figi).append('\'');
+        sb.append(", security_name='").append(security_name).append('\'');
+        sb.append(", market_sector='").append(market_sector).append('\'');
+        sb.append(", security_type='").append(security_type).append('\'');
+        sb.append(", stock_exchange='").append(stock_exchange).append('\'');
+        sb.append(", last_crsp_adj_date='").append(last_crsp_adj_date).append('\'');
+        sb.append(", composite_figi='").append(composite_figi).append('\'');
+        sb.append(", figi_uniqueid='").append(figi_uniqueid).append('\'');
+        sb.append(", share_class_figi='").append(share_class_figi).append('\'');
+        sb.append(", figi_exch_cntry='").append(figi_exch_cntry).append('\'');
+        sb.append(", currency='").append(currency).append('\'');
+        sb.append(", mic='").append(mic).append('\'');
+        sb.append(", exch_symbol='").append(exch_symbol).append('\'');
+        sb.append(", etf=").append(etf);
+        sb.append(", delisted_security=").append(delisted_security);
+        sb.append(", primary_listing=").append(primary_listing);
+        sb.append('}');
+        return sb.toString();
+    }
 }
